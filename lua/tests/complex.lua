82120d3f5c1b0deca9136a5f3128578bbd77aefc
Laurent Deniau <laurent.deniau@gmail.com>
2015-06-26 09:33:04 +0200

    typo for default init corrected

--- lua/tests/complex.lua
+++ lua/tests/complex.lua
@@ -1,7 +1,8 @@
+package.path = '../?.lua;' .. package.path
 local complex = require 'complex'
 
 local a, b = (1+1i)/(math.sqrt(2)+1e-8), 1
-local n = arg[1] and tonumber(arg[1]) or 1e-8
+local n = arg[1] and tonumber(arg[1]) or 1e8
 
 for i=1,n do
 	b = b * a
