730a0ff54a75ef96be874950570b17e74a22396b
Laurent Deniau <laurent.deniau@gmail.com>
2016-01-04 14:55:42 +0100

    update

--- lua/tests/vector.lua
+++ lua/tests/vector.lua
@@ -1,5 +1,5 @@
 local complex = require 'complex'
-local vector  = require 'vector'
+local matrix  = require 'matrix'
 local gmath   = require 'gmath'
 
 local sqrt, tostring = gmath.sqrt, gmath.tostring
@@ -7,8 +7,8 @@ local sqrt, tostring = gmath.sqrt, gmath.tostring
 local n = arg[1] and tonumber(arg[1]) or 1e7
 
 local I = 1 -- complex(0,1)
-local a = vector {1.0001, 1.0002, 1.0003, 1.0004}
-local b = vector {I,I,I,I}
+local a = matrix {{1.0001, 1.0002, 1.0003, 1.0004}}
+local b = matrix {{I,I,I,I}}
 
 for i=1,n do
 	b = a * b + b - a
@@ -16,7 +16,7 @@ end
 
 io.write(tostring(b), '\n')
 
-local x = vector {1,0,0}
-local y = vector {0,1,0}
+local x = matrix {{1,0,0}}
+local y = matrix {{0,1,0}}
 
 print( x:angle(y), y:angle(x) )
