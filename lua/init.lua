47c9be860364524e35d7c53e7c3ada4274aa02fc
Laurent Deniau <laurent.deniau@cern.ch>
2016-01-14 15:04:08 +0100

    bit implementation moved and improved

--- lua/init.lua
+++
@@ -1,1 +0,0 @@
-local M = { __author = 'ldeniau', __version = '2015.06', __help = {}, __test = {} }
--- src/mad_bit.h
+++ src/mad_bit.h
@@ -38,13 +38,129 @@ static bit_t mad_bit_get     (bit_t b, int n);
 static bit_t mad_bit_clr     (bit_t b, int n);
 static bit_t mad_bit_add     (bit_t a, bit_t b);
 static bit_t mad_bit_trunc   (bit_t b, int n);
-static bit_t mad_bit_lowest  (bit_t b);
-static bit_t mad_bit_highest (bit_t b);
+static int   mad_bit_lowest  (bit_t b);
+static int   mad_bit_highest (bit_t b);
+
+#endif // MAD_BIT_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // --- implementation (private) ----------------------------------------------o
 
-#include "mad_bit_impl.h"
+#ifndef MAD_BIT_IMPL_H
+#define MAD_BIT_IMPL_H
+
+// http://graphics.stanford.edu/~seander/bithacks.html
+
+static inline bit_t
+mad_bit_set (bit_t b, int n)
+{
+  return b | (1u << n);
+}
+
+static inline bit_t
+mad_bit_get (bit_t b, int n)
+{
+  return b & (1u << n);
+}
+
+static inline bit_t
+mad_bit_clr (bit_t b, int n)
+{
+  return b & ~(1u << n);
+}
+
+static inline bit_t
+mad_bit_add (bit_t a, bit_t b)
+{
+  return a | b;
+}
+
+static inline bit_t
+mad_bit_trunc (bit_t b, int n)
+{
+  return b & ((2u << n) - 1);
+}
+
+static inline int
+mad_bit_lowest (bit_t b)
+{
+  static const unsigned char tbl[32] = {
+    0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+    31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
+  };
+  return b ? tbl[((b & -b) * 0x077CB531u) >> 27] : 32;
+}
+
+static inline int
+mad_bit_highest2 (bit_t b)
+{
+  #define R2(n)    n,     n + 2*64,     n + 1*64,     n + 3*64
+  #define R4(n) R2(n), R2(n + 2*16), R2(n + 1*16), R2(n + 3*16)
+  #define R6(n) R4(n), R4(n + 2*4 ), R4(n + 1*4 ), R4(n + 3*4 )
+  static const unsigned char tbl[256] = { R6(0), R6(2), R6(1), R6(3) };
+  #undef  R2
+  #undef  R4
+  #undef  R6
+  bit_t r = (tbl[ b        & 0xff] << 24) | 
+            (tbl[(b >>  8) & 0xff] << 16) | 
+            (tbl[(b >> 16) & 0xff] <<  8) |
+            (tbl[(b >> 24) & 0xff]);
+  return 31 - mad_bit_lowest(r);
+}
 
 // ---------------------------------------------------------------------------o
 
-#endif // MAD_BIT_H
+#endif // MAD_BIT_IMPL_H
