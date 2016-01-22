9a64926a4ab46374547ece300028910aa60343ac
Laurent Deniau <laurent.deniau@cern.ch>
2016-01-22 18:58:45 +0100

    map flow dev - step 1

--- src/mflow.lua
+++ src/mflow.lua
@@ -45,9 +45,10 @@ SEE ALSO
 
 -- modules -------------------------------------------------------------------o
 
-local gmath = require 'gmath'
-local tpsa  = require 'tpsa'
-local xtpsa = require 'xtpsa'
+local gmath   = require 'gmath'
+local tpsa    = require 'tpsa'
+local xtpsa   = require 'xtpsa'
+local complex = require 'complex'
 
 local desc  = xtpsa.desc
 local mono  = xtpsa.mono
@@ -55,7 +56,8 @@ local mono  = xtpsa.mono
 -- locals --------------------------------------------------------------------o
 
 local type, insert = type, table.insert
-local isnum = gmath.is_number
+local isnum, iscpx, isscl, ident =
+      gmath.is_number, gmath.is_complex, gmath.is_scalar, gmath.ident
 
 local D = {}  -- private desc
 local V = {}  -- private keys
@@ -66,7 +68,7 @@ local S_dft = {'x', 'px', 'y', 'py', 't', 'pt'} -- default variable names
 
 -- implementation ------------------------------------------------------------o
 
-local function make_map(args, ctor)
+local function make_map(args, num_ctor, tpsa_ctor)
   if args.v then
     for _,v in ipairs(args.v) do
       assert(S_lst[v], "invalid variable name")
@@ -81,25 +83,22 @@ local function make_map(args, ctor)
   for i=1,dsc.nmv do
     map[V][i] = var[i]
     if mo[i] == 0 then
-      map[V][var[i]] = 0
+      map[V][var[i]] = num_ctor(0)
     else
-      map[V][var[i]] = ctor(dsc, mo[i])
+      map[V][var[i]] = tpsa_ctor(dsc, mo[i])
     end
   end
 
   return setmetatable(map, M)
 end
 
-local function mflow(args)
-  return make_map(args, tpsa)
+local function map(args)
+  return make_map(args, ident, tpsa)
 end
 
-local function cmflow(args)
-  return make_map(args, ctpsa)
+local function cmap(args)
+  return make_map(args, complex, ctpsa)
 end
-
-M.mflow  = mflow
-M.cmflow = cmflow
 
 -- indexing ------------------------------------------------------------------o
 
@@ -110,31 +109,22 @@ end
 
 function M.__newindex (tbl, key, val)
 --  io.write("setting ", key, '\n')
-  local K   = tbl[V][key] and V or T
-  local var = tbl[K][key]
+  local K = tbl[V][key] and V or T
+  local v = tbl[K][key]         -- get the variable
 
-  if var == nil then
-    tbl[K][#tbl[K]+1] = key           -- save the name
-    if isnum(val) then
-      tbl[K][key] = val               -- create number
-    else
-      tbl[K][key] = val:set_var()     -- create TPSA
-    end
-
-  elseif isnum(var) then
-    if isnum(val) then
-      tbl[K][key] = val               -- number -> number
-    else
-      tbl[K][key] = val.coef[0]       -- TPSA -> number
-      val:release()
-    end
-  elseif isnum(val) then
-    tpsa.scalar(var, val)             -- number -> TPSA
+  if v == nil then              -- create the variable
+    tbl[K][#tbl[K]+1] = key
+    tbl[K][key]       = val
+  elseif isscl(v) then          -- number or tpsa -> number
+    tbl[K][key] = isscl(val) and val or val.coef[0]
+  elseif isscl(val) then
+    v:scalar(val)               -- number -> TPSA
   else
-    tpsa.copy(val, var)               -- TPSA -> TPSA
-    val:release()
+    val:copy(v)                 -- TPSA -> TPSA
   end
 end
+
+-- TODO
 
 function M.clear(tbl)
   tbl = tbl[T]
@@ -163,31 +153,34 @@ function M.print(tbl)
   end
 end
 
-function M:to(...)
-  if not self[D] then return end
+function M.to(tbl, ...)
+  if not tbl[D] then return end
   local to, mo = -1
   for _,v in ipairs{...} do
     mo = isnum(v) and 0 or v.mo
     to = mo > to and mo or to
   end
 
-  tpsa.gtrunc(self[D],to)
+  tpsa.gtrunc(tbl[D],to)
 end
 
-function M.set(m, var, mono, val)
-  if isnum(m[var]) then
+function M.set(tbl, var, mono, val)
+  if isnum(tbl[var]) then
     assert(mono_sum(mono) == 0, "Invalid set for constant var")
-    m[var] = val
+    tbl[var] = val
   else
-    m[var]:set(mono, val)
+    tbl[var]:set(mono, val)
   end
 end
 
-function M.get(m, var, mono)
-  return isnum(m[var]) and assert(mono_sum(mono) == 0, "Invalid get") and m[var]
-         or m[var]:get(mono)
+function M.get(tbl, var, mono)
+  return isnum(tbl[var]) and assert(mono_sum(mono) == 0, "Invalid get") and tbl[var]
+         or tbl[var]:get(mono)
 end
 
 ------------------------------------------------------------------------------o
 
-return mflow
+return {
+  map  = map,
+  cmap = cmap
+}
