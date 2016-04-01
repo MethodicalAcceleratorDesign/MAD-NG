8b4745e4eb631ecfad242cddfc676cb4d417e417
Laurent Deniau <laurent.deniau@cern.ch>
2016-04-01 18:35:48 +0200

    typo corrected in rect

--- src/mad_num.c
+++ src/mad_num.c
@@ -64,7 +64,7 @@ void mad_cnum_pow_r (num_t x_re, num_t x_im, num_t y_re, num_t y_im, cnum_t *r)
 { CHKR; *r = cpow( CNUM(x), CNUM(y) ); }
 
 void mad_cnum_rect_r (num_t rho, num_t ang, cnum_t *r)
-{ CHKR; *r = CNUM2( rho * sin(ang), rho * sin(ang) ); }
+{ CHKR; *r = CNUM2( rho * cos(ang), rho * sin(ang) ); }
 
 void mad_cnum_polar_r (num_t x_re, num_t x_im, cnum_t *r)
 { CHKR; *r = CNUM2( cabs(CNUM(x)), carg(CNUM(x)) ); }
