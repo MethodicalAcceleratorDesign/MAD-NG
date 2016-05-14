f1d281ce25511e8bb00da67a37874364a33d4504
Laurent Deniau <laurent.deniau@cern.ch>
2016-05-14 18:26:35 +0200

    xrange implemented

--- tests/xrange_spd2.lua
+++ tests/xrange_spd2.lua
@@ -1,16 +1,16 @@
-local jit = require 'jit'
-local jitv   = require('jit.v')
+--local jit = require 'jit'
+--local jitv   = require('jit.v')
 -- jit.opt.start("sizemcode=256","loopunroll=25")
-jitv.start()
+--jitv.start()
 --jit.off()
 
 local range = require 'xrange'
 
 local a = 0
 for i=1,100 do
-  local r = 0..2
+  local r = 0..10
   a = a + r:len()
-  print(i, r)
-  print(r.start, r.stop, r.step)
+--  print(i, r)
+--  print(r.start, r.stop, r.step)
 end
 print(a)
