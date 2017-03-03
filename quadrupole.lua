72efe0ae65458fb910629ae2ca8ac5adbf5af581
Thomas Gläßle <t_glaessle@gmx.de>
2017-03-03 11:17:48 +0100

    Deprecate merge() in favor of MAD.Object

--- quadrupole.lua
+++ quadrupole.lua
@@ -4,7 +4,7 @@ return DEFAULTS {
   k1      = 0.1,
   -- output:
   prefix = 'quadrupole/',
-  studies = merge(DEFAULTS.studies, {
+  studies = DEFAULTS.studies {
     k1 = {stop=0.01, x=0.01, y=0.01},
-  }),
+  },
 }
