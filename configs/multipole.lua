eb0d71ff77829a836c40fa3fef51b9120e39286c
Thomas Gläßle <t_glaessle@gmx.de>
2017-03-07 22:54:53 +0100

    Temp workaround for multipole [REVERT LATER]

--- configs/multipole.lua
+++ configs/multipole.lua
@@ -1,9 +1,17 @@
 return DEFAULTS {
+  mad_sequence = [[
+local sequence = MAD.sequence "${seq_name}" {
+  refer="entry", l=${seq_len},
+  MAD.element.drift "dr" {l=0.5, at=0},
+  MAD.element.${el_type} "${el_name}" { ${el_args} },
+}
+  ]],
   el_type = "multipole",
   el_args = [[knl:={k0l, k1l, k2l}, at=0.5]],
   k0l     = 0,
   k1l     = 0,
   k2l     = 0,
+  observe = '#e',
   -- output:
   prefix = 'multipole/',
   studies = DEFAULTS.studies {
