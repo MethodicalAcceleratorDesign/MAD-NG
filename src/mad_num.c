f24a068d712df23f62a3267c066ed1f25eef79b2
Laurent Deniau <laurent.deniau@gmail.com>
2017-04-07 01:05:27 +0200

    add new number generator xorshift1024star

--- src/mad_num.c
+++ src/mad_num.c
@@ -84,7 +84,7 @@ void mad_cnum_ipow_r (num_t x_re, num_t x_im, long long y, cnum_t *r)
   }
 }
 
-// --- Faddeeva function and variants from MIT
+// --- Faddeeva function and variants from MIT --------------------------------o
 
 #include "Faddeeva.h"
 
@@ -105,3 +105,82 @@ void mad_cnum_erfcx_r (num_t x_re, num_t x_im, num_t relerr, cnum_t *r)
 
 void mad_cnum_dawson_r (num_t x_re, num_t x_im, num_t relerr, cnum_t *r)
 { CHKR; *r = Faddeeva_Dawson (CNUM(x), relerr); }
+
+// -- RNG XorShift1024* -------------------------------------------------------o
+
+#define N 16
+
+struct rng_state {
+  u64_t s[N];
+  int   p;
+};
+
+union numbit {
+  u64_t u;
+  num_t d;
+};
+
+u64_t mad_num_irand (rng_state_t *restrict st)
+{
+  int p = st->p;
+  u64_t *s = st->s;
+  const u64_t s0 = s[p];
+  u64_t s1 = s[p = (p + 1) & (N - 1)];
+  s1 ^= s1 << 31; // A
+  s[p] = s1 ^ s0 ^ (s1 >> 11) ^ (s0 >> 30); // B, C
+  st->p = p;
+  return s[p] * 1181783497276652981ULL; // number within [0,ULLONG_MAX]
+}
+
+num_t mad_num_rand (rng_state_t *restrict st)
+{
+  u64_t r = mad_num_irand(st);
+  r = (r & 0x000fffffffffffffULL) | 0x3ff0000000000000ULL;
+  const union numbit n = { .u = r }; // number within [1.,2.)
+  return n.d - 1.0;                  // number within [0.,1.)
+}
+
+void mad_num_randseed (rng_state_t *restrict st, num_t seed)
+{
+  u64_t *s = st->s;
+  const union numbit n = { .d = seed ? seed : M_PI };
+  for (int i = 0; i < N; i++)
+    s[i] = n.u * 33;
+  for (int i = 0; i < N; i++)
+    mad_num_irand(st);
+}
+
+void mad_num_randjump (      rng_state_t *restrict st,
+                       const rng_state_t *restrict ref)
+{
+  static const u64_t jump[N] = {
+    0x84242f96eca9c41dULL, 0xa3c65b8776f96855ULL, 0x5b34a39f070b5837ULL,
+    0x4489affce4f31a1eULL, 0x2ffeeb0a48316f40ULL, 0xdc2d9891fe68c022ULL,
+    0x3659132bb12fea70ULL, 0xaac17d8efa43cab8ULL, 0xc4cb815590989b13ULL,
+    0x5ee975283d71c93bULL, 0x691548c86c1bd540ULL, 0x7910c41d10a1e6a5ULL,
+    0x0b5fc64563b3e2a8ULL, 0x047f7684e9fc949dULL, 0xb99181f2d8f685caULL,
+    0x284600e3f30e38c3ULL
+  };
+
+  if (ref)
+    for (int i = 0; i < N; i++) {
+      st->s[i] = ref->s[i];
+      st->p    = ref->p;
+    }
+
+  int p = st->p;
+  u64_t *s = st->s;
+  u64_t t[N] = { 0 };
+
+  for(int i = 0; i < N; i++)
+    for(int b = 0; b < 64; b++) {
+      if (jump[i] & 1ULL << b)
+        for(int j = 0; j < N; j++)
+          t[j] ^= s[(j + p) & (N - 1)];
+      mad_num_irand(st);
+    }
+  for(int j = 0; j < N; j++)
+    s[(j + p) & (N - 1)] = t[j];
+}
+
+#undef N
