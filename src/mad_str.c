e62710714383a537b555ad089c6a974a246e07a6
Laurent Deniau <laurent.deniau@gmail.com>
2017-12-31 02:26:01 +0100

    review of fast string functions, update sequence and mtable accordingly

--- src/mad_str.c
+++ src/mad_str.c
@@ -23,64 +23,116 @@
 
 // --- implementation ---------------------------------------------------------o
 
-str_t
-mad_str_trim (str_t str, ssz_t len[2])
+static inline void
+mad_str_trim_front (str_t str, ssz_t arg[2])
 {
-  assert(str && len);
+  while (arg[1] > 0 && isspace(str[arg[0]])) --arg[1], ++arg[0];
+}
 
-  while (len[1] > 0 && isspace(str[len[0]         ])) --len[1], ++len[0];
-  while (len[1] > 0 && isspace(str[len[0]+len[1]-1])) --len[1];
+static inline void
+mad_str_trim_back (str_t str, ssz_t arg[2])
+{
+  while (arg[1] > 0 && isspace(str[arg[0]+arg[1]-1])) --arg[1];
+}
+
+str_t
+mad_str_trim (str_t str, ssz_t arg[2])
+{
+  assert(str && arg);
+  mad_str_trim_front(str, arg);
+  mad_str_trim_back (str, arg);
+  return str;
+}
+
+str_t
+mad_str_quote (str_t str, ssz_t arg[4])
+{
+  assert(str && arg);
+  mad_str_trim_front(str, arg);
+
+  if (str[arg[0]] != '"' && str[arg[0]] != '\'') { // no quote found
+    arg[2] = -1, arg[3] = 0;
+    return str;
+  }
+
+  ssz_t i = arg[0], j = i+1, k = arg[0]+arg[1];
+
+  if (str[i] == '"') while (j < k && str[j] != '"' ) ++j;
+  else               while (j < k && str[j] != '\'') ++j;
+
+  if (j == k) return NULL; // error: no closing quote found
+
+  arg[0] = i+1;
+  arg[1] = j-(i+1);
+  arg[2] = j;
+  arg[3] = (str[i] == '\'') + 1;
 
   return str;
 }
 
 str_t
-mad_str_split (str_t str, ssz_t len[5], str_t sep)
+mad_str_bracket (str_t str, ssz_t arg[6])
 {
-  assert(str && len && sep);
+  assert(str && arg);
+  mad_str_trim_front(str, arg);
 
-  ssz_t i = len[0], k = len[0]+len[1];
-  while (i < k && str[i] != *sep) ++i;
+  ssz_t i = arg[0], k = arg[0]+arg[1];
 
-  if (i == k) // no sep found
-    len[2] = len[3] = len[4] = -1;
-  else {
-    len[1] = i-len[0];
-    len[2] = i+1;
-    len[3] = k-(i+1);
-    len[4] = i;
-    mad_str_trim(str, len+2);
-  }
-
-  return mad_str_trim(str, len);
-}
-
-str_t
-mad_str_split_bracket (str_t str, ssz_t len[6])
-{
-  assert(str && len);
-
-  ssz_t i = len[0], k = len[0]+len[1];
   while (i < k && str[i] != '[' && str[i] != '{'
                && str[i] != ']' && str[i] != '}') ++i;
 
-  ssz_t j = k-1;
-       if (str[i] == '[') while (j > i && str[j] != ']') --j;
-  else if (str[i] == '{') while (j > i && str[j] != '}') --j;
-  else j = i;
-
-       if (i == k) // no bracket found
-    len[2] = len[3] = len[4] = len[5] = -1;
-  else if (i == j) // bad pairs of brackets
-    len[2] = len[3] = len[5] = -1, len[4] = i;
-  else {
-    len[1] = i-len[0];
-    len[2] = i+1;
-    len[3] = j-(i+1);
-    len[4] = i;
-    len[5] = j;
-    mad_str_trim(str, len+2);
+  if (i == k) { // no bracket found
+    arg[2] = arg[3] = arg[4] = -1, arg[5] = 0;
+    return str;
   }
 
-  return mad_str_trim(str, len);
+  if (str[i] == ']' || str[i] == '}') // error: no opening bracket
+    return NULL;
+
+  ssz_t j = i+1;
+  while (j < k && str[j] != '[' && str[j] != '{'
+               && str[j] != ']' && str[j] != '}') ++j;
+
+  if (str[j] == '[' || str[j] == '{') // error: no closing bracket
+    return NULL;
+
+  arg[1] = i-arg[0];
+  arg[2] = j;
+  arg[3] = i+1;
+  arg[4] = j-(i+1);
+  arg[5] = (str[i] == '{') + 1;
+  mad_str_trim_back(str, arg  );
+  mad_str_trim     (str, arg+3);
+
+  return str;
+}
+
+str_t
+mad_str_split (str_t str, ssz_t arg[4], str_t sep)
+{
+  assert(str && arg && sep);
+  mad_str_trim_front(str, arg);
+
+  ssz_t i = arg[0], j = -1, k = arg[0]+arg[1], l = arg[2];
+
+  switch(l) {
+    case 1: while (i < k && str[i] != sep[j=0]) ++i; break;
+    case 2: while (i < k && str[i] != sep[j=0] && str[i] != sep[j=1]) ++i; break;
+    case 3: while (i < k && str[i] != sep[j=0] && str[i] != sep[j=1] && str[i] != sep[j=2]) ++i; break;
+    case 4: while (i < k && str[i] != sep[j=0] && str[i] != sep[j=1] && str[i] != sep[j=2] && str[i] != sep[j=3]) ++i; break;
+    default: for (; i < k; ++i) for (j=0; j<l; ++j) if (str[i] == sep[j]) goto found;
+  }
+
+found:
+  if (i == k) { // no sep found
+    arg[2] = arg[3] = -1;
+    return str;
+  }
+
+  arg[1] = i-arg[0];
+  arg[2] = i;
+  arg[3] = j;
+  mad_str_trim_back(str, arg);
+
+  return str;
 }
