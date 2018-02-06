d07ce714b3045d20b3d910b4ee7fc05c1caa7460
Laurent DENIAU <laurent.deniau@gmail.com>
2018-02-06 11:43:51 +0100

    cleanup

--- src/mad_mono.c
+++ src/mad_mono.c
@@ -95,15 +95,6 @@ mad_mono_max (int n, const ord_t a[n])
 }
 
 int
-mad_mono_eq (int n, const ord_t a[n], const ord_t b[n])
-{
-  assert(a && b);
-  for (int i=0; i < n; ++i)
-    if (a[i] != b[i]) return 0;
-  return 1;
-}
-
-int
 mad_mono_rcmp (int n, const ord_t a[n], const ord_t b[n])
 {
   assert(a && b);
@@ -138,6 +129,15 @@ mad_mono_print (int n, const ord_t m[n])
 // --- default versions ------------------------------------------------------o
 
 int
+mad_mono_eq (int n, const ord_t a[n], const ord_t b[n])
+{
+  assert(a && b);
+  for (int i=0; i < n; ++i)
+    if (a[i] != b[i]) return 0;
+  return 1;
+}
+
+int
 mad_mono_lt (int n, const ord_t a[n], const ord_t b[n])
 {
   assert(a && b);
@@ -159,6 +159,8 @@ mad_mono_le (int n, const ord_t a[n], const ord_t b[n])
 
 #elif defined(__AVX512F__) && defined(AVX512BW)
 #include "sse/mad_mono_avx512.tc"
+#elif defined(__AVX2__)
+#include "sse/mad_mono_avx2.tc"
 #else
 #include "sse/mad_mono_sse.tc"
 #endif // __SSE2__ || __AVX512F__
