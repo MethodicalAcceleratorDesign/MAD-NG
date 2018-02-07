358a4ccc95e7ccf314bfdf3558033770c53a5b50
Laurent DENIAU <laurent.deniau@gmail.com>
2018-02-07 23:01:37 +0100

    rename maxsize into maxlen, update api of desc ctor

--- src/mad_desc.h
+++ src/mad_desc.h
@@ -34,29 +34,29 @@
 #include "mad_defs.h"
 #include "mad_mono.h"
 
-// --- types -----------------------------------------------------------------o
+// --- types ------------------------------------------------------------------o
 
 typedef struct desc desc_t;
 
-// --- globals ---------------------------------------------------------------o
+// --- globals ----------------------------------------------------------------o
 
 extern const ord_t mad_tpsa_default;
 extern const ord_t mad_tpsa_same;
 extern       int   mad_tpsa_strict;
 
-// --- interface -------------------------------------------------------------o
+// --- interface --------------------------------------------------------------o
 
 // ctors, dtor
-desc_t* mad_desc_new (int nmv, const ord_t mvar_ords[nmv], str_t mvar_names_[nmv]);
-desc_t* mad_desc_newv(int nmv, const ord_t mvar_ords[nmv], str_t mvar_names_[nmv],
-                      int nv , const ord_t  var_ords[nv ], ord_t dk);
-void    mad_desc_del (desc_t *d);
+desc_t* mad_desc_new  (int nmv, const ord_t mvar_ords[nmv], str_t mvar_names_[nmv]);
+desc_t* mad_desc_newv (int nmv, const ord_t mvar_ords[nmv], str_t mvar_names_[nmv],
+                       int nv , const ord_t  var_ords[nv ], ord_t dk); // knobs X-order
+void    mad_desc_del  (desc_t *d);
 
 // introspection
-int     mad_desc_maxsize (const desc_t *d);
-ord_t   mad_desc_maxord  (const desc_t *d);
-ord_t   mad_desc_gtrunc  (      desc_t *d, ord_t to);
+int     mad_desc_maxlen (const desc_t *d);
+ord_t   mad_desc_maxord (const desc_t *d);
+ord_t   mad_desc_gtrunc (      desc_t *d, ord_t to);
 
-// ---------------------------------------------------------------------------o
+// --- end --------------------------------------------------------------------o
 
 #endif // MAD_DESC_H
