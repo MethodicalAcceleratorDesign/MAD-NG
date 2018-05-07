e81a01a2f888f65fc00a88c1d83adcc38e906b8f
Laurent DENIAU <laurent.deniau@gmail.com>
2018-05-07 15:45:58 +0200

    cleanup, handle multiple strict/unstrict with different extra

--- src/madl_strict.lua
+++ src/madl_strict.lua
@@ -2,8 +2,9 @@
 -- http://www.lua.org/pil/14.2.html
 -- http://lua-users.org/wiki/DetectingUndefinedVariables
 
+local IGNORED_EXTRAS
 local IGNORED_WRITES = {}
-local IGNORED_READS = {
+local IGNORED_READS  = {
   _PROMPT=true,
   _PROMPT2=true,
 }
@@ -13,36 +14,45 @@ local MT = {
     if IGNORED_READS[key] then
       return
     end
-    error("attempt to read undeclared variable "..key, 2)
+    error("attempt to read undeclared variable: "..key, 2)
   end,
 
   __newindex = function (table, key, value)
-    if not IGNORED_WRITES[key] then
+    if not (IGNORED_WRITES[key] or IGNORED_EXTRAS[key]) then
       local info = debug.getinfo(2, "Sl")
-      io.stderr:write(string.format(
-        "strict: %s:%s: write to undeclared variable: %s\n",
-        tostring(info.short_src), tostring(info.currentline), key))
+      MAD.warn("%s:%s: write to undeclared global variable: %s\n",
+               tostring(info.short_src), tostring(info.currentline), key)
     end
     rawset(table, key, value)
   end,
 }
 
-local origRequire = require
+local require_orig = require
 
--- Raises an error when an undeclared variable is read.
-local function strict ()
-  assert(getmetatable(_G) == nil, "another global metatable exists")
-  setmetatable(_G, MT)
-  function require(modname)
-    IGNORED_WRITES[modname] = true
-    return origRequire(modname)
+local function require_mod (modname)
+  IGNORED_WRITES[modname] = true
+  return origRequire(modname)
+end
+
+-- Raises an error when an undeclared variable is read or written.
+local function strict (extra)
+  local mt = getmetatable(_G)
+  if mt ~= nil and mt ~= MT then
+    error("a global metatable already exists")
   end
+  setmetatable(_G, MT)
+  IGNORED_EXTRAS = extra or {}
+  require = require_mod
 end
 
 local function unstrict ()
-  assert(getmetatable(_G) == MT, "invalid global metatable")
+  local mt = getmetatable(_G)
+  if mt ~= nil and mt ~= MT then
+    error("invalid global metatable (i.e. not set by strict)")
+  end
   setmetatable(_G, nil)
-  require = origRequire
+  IGNORED_EXTRAS = nil
+  require = require_orig
 end
 
 return {
