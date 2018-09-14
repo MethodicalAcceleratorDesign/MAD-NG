12e068f54d1a23d0fa23bbdf780e955c8127dc55
Laurent DENIAU <laurent.deniau@gmail.com>
2018-09-14 15:34:43 +0200

    add nlopt generic C interface (i.e. all in one)

--- src/mad_nlopt.c
+++ src/mad_nlopt.c
@@ -19,70 +19,48 @@
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
-#include <nlopt.h>
+#include "mad_log.h"
 #include "mad_nlopt.h"
 
-// FIRST EXAMPLE
-
-typedef struct {
-    double a, b;
-} my_constraint_data;
-
-static double myfunc(unsigned n, const double *x, double *grad, void *data)
+void mad_nlopt (nlopt_args_t *a)
 {
-  assert(n == 2);
-  assert(x && data);
-  ++*(int*)data;
+  assert(a);
+  ensure(          a->n > 0 && a->f    && a->x       , "invalid objective function");
+  ensure(!a->p || (a->p > 0 && a->c_eq && a->ctol_eq), "invalid equality constraints");
+  ensure(!a->q || (a->q > 0 && a->c_le && a->ctol_le), "invalid inequality constraints");
 
-  if (grad) {
-      grad[0] = 0.0;
-      grad[1] = 0.5 / sqrt(x[1]);
-  }
+  // create optimizer, set algorithm and problem dimension
+  nlopt_opt opt = nlopt_create(a->algorithm, a->n);
 
-  return sqrt(x[1]);
-}
+  // set objective function to minimize
+  nlopt_set_min_objective(opt, a->f, a->fdat);
 
-static double myconstraint(unsigned n, const double *x, double *grad, void *data)
-{
-  assert(n == 2);
-  assert(x && data);
-  my_constraint_data *d = (my_constraint_data *) data;
+  // set contraint functions to satisfy withing tolerances
+  if (a->p)
+    nlopt_add_equality_mconstraint  (opt, a->p, a->c_eq, a->cdat_eq, a->ctol_eq);
+  if (a->q)
+    nlopt_add_inequality_mconstraint(opt, a->q, a->c_le, a->cdat_le, a->ctol_le);
 
-  double a = d->a, b = d->b;
+  // set variables boundary constraints
+  if (a->xmin) nlopt_set_lower_bounds(opt, a->xmin);
+  if (a->xmax) nlopt_set_upper_bounds(opt, a->xmax);
 
-  if (grad) {
-      grad[0] = 3 * a * (a*x[0] + b) * (a*x[0] + b);
-      grad[1] = -1.0;
-  }
+  // set variables intial steps
+  if (a->xstep) nlopt_set_initial_step(opt, a->xstep);
 
-  return ((a*x[0] + b) * (a*x[0] + b) * (a*x[0] + b) - x[1]);
- }
+  // set variables tolerances
+  if (a->xtol_rel) nlopt_set_xtol_rel(opt, *a->xtol_rel);
+  if (a->xtol_abs) nlopt_set_xtol_abs(opt,  a->xtol_abs);
 
-void mad_nlopt (void)
-{
-  nlopt_opt opt = nlopt_create(NLOPT_LD_MMA, 2); /* algorithm and dimensionality */
+  // set objective function tolerances
+  if (a->ftol_rel) nlopt_set_ftol_rel(opt, *a->ftol_rel);
+  if (a->ftol_abs) nlopt_set_ftol_abs(opt, *a->ftol_abs);
 
-  double lb[2] = { -HUGE_VAL, 0 }; /* lower bounds */
-  nlopt_set_lower_bounds(opt, lb);
+  // set abnormal stopping criteria
+  if (a->maxcall) nlopt_set_maxeval(opt, *a->maxcall);
+  if (a->maxtime) nlopt_set_maxtime(opt, *a->maxtime);
 
-  int count = 0;
-  nlopt_set_min_objective(opt, myfunc, &count);
-
-  my_constraint_data data[2] = { {2,0}, {-1,1} };
-  nlopt_add_inequality_constraint(opt, myconstraint, &data[0], 1e-12);
-  nlopt_add_inequality_constraint(opt, myconstraint, &data[1], 1e-12);
-
-  nlopt_set_xtol_rel(opt, 1e-6);
-
-  double x[2] = { 1.234, 5.678 };  /* `*`some` `initial` `guess`*` */
-  double minf; /* `*`the` `minimum` `objective` `value,` `upon` `return`*` */
-
-  if (nlopt_optimize(opt, x, &minf) < 0) {
-      printf("nlopt failed!\n");
-  }
-  else {
-      printf("found minimum at f(%g,%g) = %0.10g in %d iterations\n", x[0], x[1], minf, count);
-  }
+  a->status = nlopt_optimize(opt, a->x, &a->fmin);
 
   nlopt_destroy(opt);
 }
--- src/mad_nlopt.h
+++ src/mad_nlopt.h
@@ -24,8 +24,64 @@
  o-----------------------------------------------------------------------------o
  */
 
+#include <nlopt.h>
 #include "mad_defs.h"
 
-void mad_nlopt (void);
+// -- interface ---------------------------------------------------------------o
+
+typedef struct nlopt_args nlopt_args_t;
+
+void mad_nlopt (nlopt_args_t *args);
+
+// -- types -------------------------------------------------------------------o
+
+// Define explicitly nlopt_func and nlopt_mfunc here to ensure FFI x-check
+typedef num_t (objective_t )(u32_t n, const num_t *x, num_t *grad, void *data);
+typedef void  (constraint_t)(u32_t m, num_t *res,
+                             u32_t n, const num_t* x, num_t* grad, void* data);
+
+struct nlopt_args {
+  // algorithm
+  int             algorithm;
+
+  // variables [n] (required)
+  num_t          *x;
+  const num_t    *xtol_rel;
+  const num_t    *xtol_abs;
+  const num_t    *xstep;
+
+  // objective function [n] (required)
+  ssz_t           n;
+  objective_t    *f;
+  void           *fdat;
+  const num_t    *ftol_rel;
+  const num_t    *ftol_abs;
+
+  // equality constraints [p] (optional)
+  ssz_t           p;
+  constraint_t   *c_eq;
+  void           *cdat_eq;
+  const num_t    *ctol_eq;
+
+  // inequality constraints [q] (optional)
+  ssz_t           q;
+  constraint_t   *c_le;
+  void           *cdat_le;
+  const num_t    *ctol_le;
+
+  // bounds constraint [n] (optional)
+  const num_t    *xmin;
+  const num_t    *xmax;
+
+  // stop criteria
+  const int      *maxcall;
+  const num_t    *maxtime;
+
+  // returned values
+  num_t           fmin;
+  int             status;
+};
+
+// -- end ---------------------------------------------------------------------o
 
 #endif // MAD_NLOPT_H
