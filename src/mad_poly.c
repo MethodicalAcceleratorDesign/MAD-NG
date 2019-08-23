84f71fc1b6b23fb707062633006bda9b9c0728bb
Laurent Deniau <laurent.deniau@gmail.com>
2019-08-23 17:14:29 +0200

    added winding number algorithm for generic aperure check

---
+++ src/mad_poly.c
@@ -0,0 +1,52 @@
+/*
+ o-----------------------------------------------------------------------------o
+ |
+ | Polygon module implementation
+ |
+ | Methodical Accelerator Design - Copyright CERN 2016+
+ | Support: http://cern.ch/mad  - mad at cern.ch
+ | Authors: L. Deniau, laurent.deniau at cern.ch
+ | Contrib: -
+ |
+ o-----------------------------------------------------------------------------o
+ | You can redistribute this file and/or modify it under the terms of the GNU
+ | General Public License GPLv3 (or later), as published by the Free Software
+ | Foundation. This file is distributed in the hope that it will be useful, but
+ | WITHOUT ANY WARRANTY OF ANY KIND. See http://gnu.org/licenses for details.
+ o-----------------------------------------------------------------------------o
+*/
+
+#include "mad_vec.h"
+
+// Check if a point p is inside a polygon v (vector of points)
+//   based on the winding number test for a point in a polygon
+//   code from http://geomalgorithms.com/a03-_inclusion.html
+// Input:   px,py = a point,
+//          vx,vy = vector of points of a polygon v[n] with v[n-1]=v[0]
+// Return:  wn = the winding number (=0 only when p is outside)
+
+static inline log_t
+is_left (num_t px, num_t py, const num_t *vx, const num_t *vy, ssz_t i)
+{
+  return (vx[i+1]-vx[i]) * (py-vy[i]) > (px-vx[i]) * (vy[i+1]-vy[i]);
+}
+
+static inline log_t
+is_right (num_t px, num_t py, const num_t *vx, const num_t *vy, ssz_t i)
+{
+  return (vx[i+1]-vx[i]) * (py-vy[i]) < (px-vx[i]) * (vy[i+1]-vy[i]);
+}
+
+log_t
+mad_pol_inside(num_t px, num_t py, const num_t *vx, const num_t *vy, ssz_t n)
+{
+  int wn = 0; // the winding number counter
+
+  for (ssz_t i = 0; i < n-1; i++) {
+    if (vy[i  ] <= py && vy[i+1] > py && is_left (px, py, vx, vy, i)) ++wn;
+    else
+    if (vy[i+1] <= py &&                 is_right(px, py, vx, vy, i)) --wn;
+  }
+
+  return !wn;
+}
