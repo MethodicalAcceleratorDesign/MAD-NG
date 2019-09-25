7c363e1605dee3d80b3f631d25ce8732525073fe
Laurent Deniau <laurent.deniau@gmail.com>
2019-09-25 22:10:14 +0900

    add some code to write a tpsa to a file

--- src/notmad/gtpsa_ex1.f90
+++ src/notmad/gtpsa_ex1.f90
@@ -21,17 +21,18 @@ program gtpsa_ex1
   use gtpsa
   implicit none
 
-  real(c_num_t) :: pi_6 = 3.14159265358979323846264338327950288d0/6
-  type(c_ptr)   :: d, t1, t2
-  real(c_num_t) :: vec(1:7)
+  real(c_num_t)  :: pi_6 = 3.14159265358979323846264338327950288d0/6
+  type(c_ptr)    :: d, t1, t2, fp
+  real(c_num_t)  :: vec(1:7)
+  integer(c_int) :: fe
   !integer(c_ord_t) :: no=5
 
   ! descriptor for TPSA with 6 (map) variables of order 5
-  d=mad_desc_newn(6, 5_1)
+  d = mad_desc_newn(6, 5_1)
 
   ! two TPSAs, t1 has maximum order, t2 is same as t1
-  t1=mad_tpsa_newd(d , mad_tpsa_default)
-  t2=mad_tpsa_new (t1, mad_tpsa_same)
+  t1 = mad_tpsa_newd(d , mad_tpsa_default)
+  t2 = mad_tpsa_new (t1, mad_tpsa_same)
 
   ! set order 0 and 1 (quick and dirty!)
   vec = [pi_6, 1d0,1d0,1d0,1d0,1d0,1d0]
@@ -40,13 +41,22 @@ program gtpsa_ex1
 
   ! t2=sin(t1)
   call mad_tpsa_sin  (t1, t2)
-  call mad_tpsa_print(t2, "sin"//c_eos, 0d0, c_null);
+  call mad_tpsa_print(t2, "sin"//c_eos, 1d-14, c_null);
   call mad_tpsa_del  (t1)
 
   ! tpsa functions and operators support aliasing (i.e. src == dst)
   call mad_tpsa_asin (t2, t2);           ! asin(x) = -i*ln(i*x + sqrt(1-x^2))
-  call mad_tpsa_print(t2, "asin"//c_eos, 0d0, c_null);
-  call mad_tpsa_del  (t2);               ! see the accuracy of asin(sin)
+  call mad_tpsa_print(t2, "asin"//c_eos, 1d-14, c_null);
+
+  ! write tpsa to a file with no tolerance
+  fp = mad_cio_fopen("tpsa2.txt", "w")
+  if (c_associated(fp)) then
+    call mad_tpsa_print(t2, "asin"//c_eos, 0d0, fp); ! see the accuracy of asin(sin)
+    fe = mad_cio_fclose(fp)
+  endif
+
+  ! delete tpsa 2
+  call mad_tpsa_del(t2);
 
   ! destroy all created descriptors (optional cleanup)
   call mad_desc_cleanup();
