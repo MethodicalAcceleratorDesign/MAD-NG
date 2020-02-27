c4dbeb49c42ea0a870284ed6a364b9aa125e2e00
Laurent Deniau <laurent.deniau@gmail.com>
2020-02-27 14:51:33 +0100

    remove sanity checks by default...

--- src/mad_desc_impl.h
+++ src/mad_desc_impl.h
@@ -83,7 +83,7 @@ void     mad_ctpsa_del  (const ctpsa_t *t);
 
 // --- TPSA sanity checks -----------------------------------------------------o
 
-#define DEBUG 1
+#define DEBUG 0
 
 #if DEBUG > 0
 #  define DBGTPSA(t) FUN(debug)(t,#t,__func__,__LINE__,0)
