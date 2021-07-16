55401f36166918a5351961572ba5c377b32a9ee2
Laurent Deniau <laurent.deniau@gmail.com>
2021-07-16 15:48:23 +0200

    add complex version of map operators

---
+++ src/mad_ctpsa_mops.c
@@ -0,0 +1,20 @@
+/*
+ o-----------------------------------------------------------------------------o
+ |
+ | CTPSA map exponential module implementation
+ |
+ | Methodical Accelerator Design - Copyright (c) 2016+
+ | Support: http://cern.ch/mad  - mad at cern.ch
+ | Authors: L. Deniau, laurent.deniau at cern.ch
+ | Contrib: -
+ |
+ o-----------------------------------------------------------------------------o
+ | You can redistribute this file and/or modify it under the terms of the GNU
+ | General Public License GPLv3 (or later), as published by the Free Software
+ | Foundation. This file is distributed in the hope that it will be useful, but
+ | WITHOUT ANY WARRANTY OF ANY KIND. See http://gnu.org/licenses for details.
+ o-----------------------------------------------------------------------------o
+*/
+
+#define   MAD_CTPSA_IMPL
+#include "mad_tpsa_mops.c"
