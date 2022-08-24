be01f95f7d59d870a3d810a322d7e1b529e4d6d2
jgray-19 <j.gray-19@student.lboro.ac.uk>
2022-08-24 13:20:09 +0200

    Adjusted to cope with 2D eps input Removed excess code

--- tests/utests/data/tpsaTestResultGeneration/generateTPSAFile.py
+++ tests/utests/data/tpsaTestResultGeneration/generateTPSAFile.py
@@ -42,108 +42,6 @@ def createStringOfValues(singleVarFunc, multiVarFunc, varVals, order, eps):
     else:
         return "\n"
 
-def xyzFileWrite(fName: str, f, eps, xyzValues = [Rational(1, 10), Integer(6), pi*Rational(5,3)]) -> None:
-    stringList = [  
-        f"M.axypbzpcfun.{fName} = {{\n", 
-        f"M.axyzpbfun.{fName} = {{\n", 
-        f"M.axpbypczfun.{fName} = {{\n"
-    ]
-    x, y, z = symbols("x y z")
-    a = b = c = 1
-    exprList = [
-        a * x * y + b * z + c, 
-        a * x * y * z + b * c,
-        a * x + b* y + c * z, 
-    ]
-    for xVal in xyzValues:
-        for yVal in xyzValues:
-            for zVal in xyzValues:
-                for i in range(len(stringList)):
-                    stringList[i] += createStringOfValues(f, exprList[i], [xVal, yVal, zVal], 6, eps[i])
-
-    with open(r"data/3VarTPSA.dat", "a") as file:
-        for i in range(len(stringList)):
-            stringList[i] += "}\n\n"
-            file.write(stringList[i])
-
-def vwxyFileWrite(fName: str, f, eps, vwxyValues = [Rational(1, 10), Integer(6), pi*Rational(5,3)]) -> None:
-    stringList = [  
-        f"M.axypbvwpcfun.{fName} = {{\n",
-        f"M.avwxypbfun.{fName} = {{\n",
-        f"M.avpbwpcxpdyfun.{fName} = {{\n"
-    ]
-    v, w, x, y = symbols("v w x y")
-    a = b = c = d = 1
-    exprList = [
-        a * v * w + b * x * y + c,
-        a * v * w * x * y + b,
-        a * v + b * w + c * x + d * y
-    ]
-    for vVal in vwxyValues:
-        for wVal in vwxyValues:
-            for xVal in vwxyValues:
-                for yVal in vwxyValues:
-                    for i in range(len(stringList)):
-                        stringList[i] += createStringOfValues(f, exprList[i], [vVal, wVal, xVal, yVal], 5, eps[i])
-    
-    with open(r"data/4VarTPSA.dat", "a") as file:
-        for i in range(len(stringList)):
-            stringList[i] += "}\n\n"
-            file.write(stringList[i])
-
-def vwxyzFileWrite(fName: str, f, eps, vwxyzValues = [Rational(1, 10), Integer(6), pi*Rational(5,3)]) -> None:
-    stringList = [  
-        f"M.avpbwpcxpdypezfun.{fName} = {{\n",
-        f"M.avwpbxypczfun.{fName} = {{\n",
-        f"M.avwxyzpbfun.{fName} = {{\n"
-    ]
-    v, w, x, y, z = symbols("v w x y z")
-    a = b = c = d = e = 1
-    exprList = [
-        a * v + b * w + c * x + d * y + e * z,
-        a * v * w + b * x * y + c * z, 
-        a * v * w * x * y * z + b
-    ]
-    for vVal in vwxyzValues:
-        for wVal in vwxyzValues:
-            for xVal in vwxyzValues:
-                for yVal in vwxyzValues:
-                    for zVal in vwxyzValues:
-                        for i in range(len(stringList)):
-                            stringList[i] += createStringOfValues(f, exprList[i], [vVal, wVal, xVal, yVal, zVal], 5, eps[i])
-    
-    with open(r"data/5VarTPSA.dat", "a") as file:
-        for i in range(len(stringList)):
-            stringList[i] += "}\n\n"
-            file.write(stringList[i])
-
-def tvwxyzFileWrite(fName: str, func, eps, tvwxyzValues = [Rational(1, 10), Integer(6)]):#, pi*Rational(5,3)]) -> None:
-    stringList = [  
-        f"M.atpbvpcwpdxpeypfzfun.{fName} = {{\n",
-        f"M.atvpbwxpcyzfun.{fName} = {{\n",
-        f"M.atvwxyzpbfun.{fName} = {{\n"
-    ]
-    t, v, w, x, y, z = symbols("t v w x y z")
-    a = b = c = d = e = f = 1
-    exprList = [
-        a * t + b * v + c * w + d * x + e * y + f * z,
-        a * t * v + b * w * x + c * y * z, 
-        a * t * v * w * x * y * z + b
-    ]
-    for tVal in tvwxyzValues:
-        for vVal in tvwxyzValues:
-            for wVal in tvwxyzValues:
-                for xVal in tvwxyzValues:
-                    for yVal in tvwxyzValues:
-                        for zVal in tvwxyzValues:
-                            for i in range(len(stringList)):
-                                stringList[i] += createStringOfValues(func, exprList[i], [tVal, vVal, wVal, xVal, yVal, zVal], 5, eps[i])
-    
-    with open(r"data/6VarTPSA.dat", "a") as file:
-        for i in range(len(stringList)):
-            stringList[i] += "}\n\n"
-            file.write(stringList[i])
-
 def closeFile(filename: str) -> None: #
     """Input the filename gained from creating the file and this will close off the file, allowing use with MAD
     
@@ -175,7 +73,7 @@ def generateStringsPerMultiVarFunc(multiVarIndex, multiVarExprs, singleVarFuncs,
     stringList = [""] * len(keys)
     for keyIndex in range(len(keys)): 
         for expansionPoint in expansionPoints:
-            stringList[keyIndex] += createStringOfValues(singleVarFuncs[keys[keyIndex]], multiVarExprs[multiVarIndex], expansionPoint, order, epsList[keyIndex])
+            stringList[keyIndex] += createStringOfValues(singleVarFuncs[keys[keyIndex]], multiVarExprs[multiVarIndex], expansionPoint, order, epsList[keyIndex][multiVarIndex])
     returnList[multiVarIndex] = stringList
 
 def multiVarDataGen(filename:str, singleVarFuncs: dict, multiVarExprs: list, epsList: list[int], expansionPoints: list, order: int, multiVarFnams: list[str], end: str = "") -> None:
