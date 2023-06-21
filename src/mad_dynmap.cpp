f72ffbd97e1064d67f9f6ce6970bf76ed3950f7d
Laurent Deniau <laurent.deniau@gmail.com>
2023-06-21 19:06:39 +0200

    mv sqr to hpp, update using tpsa_ref wrapper, some test

--- src/mad_dynmap.cpp
+++ src/mad_dynmap.cpp
@@ -28,23 +28,26 @@ extern "C" {
 extern "C" {
 
 struct mflw {
-  num_t el, eld;
-  int npar, T;
-  par_t *par;
-  map_t *map;
-};
-
-struct part {
-  num_t x, px, y, py, t, pt;
-  num_t beta;
-};
-
-struct damap {
-  tpsa_t *x, *px, *y, *py, *t, *pt;
-  num_t beta;
+  num_t el, eld, beta, T;
+  int    npar;
+  num_t   *par[6];
+  tpsa_t **map[6];
 };
 
 }
+
+struct par_t {
+  par_t(num_t *a, num_t b)
+    : x(a[0]), px(a[1]), y(a[2]), py(a[3]), t(a[4]), pt(a[5]), beta(b) {}
+  num_t x, px, y, py, t, pt, beta;
+};
+
+struct map_t {
+  map_t(tpsa_t **a, num_t b)
+    : x(a[0]), px(a[1]), y(a[2]), py(a[3]), t(a[4]), pt(a[5]), beta(b) {}
+  mad::tpsa_ref x, px, y, py, t, pt;
+  num_t beta;
+};
 
 // --- implementation ---------------------------------------------------------o
 
@@ -56,17 +59,12 @@ const num_t minlen = mad_cst_MINLEN;
 const num_t minang = mad_cst_MINANG;
 const num_t minstr = mad_cst_MINSTR;
 
-inline num_t sqr (       num_t  a) { return a*a; }
-inline tpsa  sqr (const tpsa_t &a) { return a*a; }
-inline tpsa  sqr (const tpsa_t *a) { return sqr(*a); }
-inline tpsa  sqr (const tpsa   &a) { return sqr(*a); }
-
 inline num_t dp2 (par_t &p) {
   return 1 + (2/p.beta)*p.pt + sqr(p.pt);
 }
 
 inline tpsa dp2 (map_t &p) {
-  return 1 + (2/p.beta)**p.pt + sqr(p.pt);
+  return 1 + (2/p.beta)*p.pt + sqr(p.pt);
 }
 
 inline num_t pz2 (par_t &p) {
@@ -74,7 +72,7 @@ inline num_t pz2 (par_t &p) {
 }
 
 inline tpsa pz2 (map_t &p) {
-  return 1 + (2/p.beta)**p.pt + sqr(p.pt) - sqr(p.px) - sqr(p.py);
+  return 1 + (2/p.beta)*p.pt + sqr(p.pt) - sqr(p.px) - sqr(p.py);
 }
 
 inline num_t dp (par_t &p) { return std::sqrt(dp2(p)); }
@@ -94,7 +92,7 @@ mad_trk_strex_drift_r (elem_t *e, mflw_t *m, num_t lw, int istp)
   int T = m->T;
 
   FOR(i,m->npar) {
-    par_t &p = m->par[i];
+    par_t p { m->par[i], m->beta };
     num_t l_pz = l/pz(p);
 
     p.x += p.px*l_pz;
@@ -113,12 +111,12 @@ mad_trk_strex_drift_t (elem_t *e, mflw_t *m, num_t lw, int istp)
   int T = m->T;
 
   FOR(i,m->npar) {
-    map_t &p = m->map[i];
+    map_t p { m->map[i], m->beta };
     const tpsa l_pz = l/pz(p);
 
-    ref(p.x) += p.px*l_pz;
-    ref(p.y) += p.py*l_pz;
-    ref(p.t) -= l_pz*(1/p.beta+*p.pt) + (1-T)*(ld/p.beta);
+    p.x += p.px*l_pz;
+    p.y += p.py*l_pz;
+    p.t -= l_pz*(1/p.beta+p.pt) + (1-T)*(ld/p.beta);
   }
 }
 
@@ -154,6 +152,97 @@ void mad_trk_slice_r (elem_t *e, mflw_t *m, num_t lw, trkfun *dft, trkfun *kck)
 void mad_trk_slice_t (elem_t *e, mflw_t *m, num_t lw, trkfun *dft, trkfun *kck)
 {
   (void)e; (void)m; (void)lw; (void)dft; (void)kck;
+}
+
+void mad_trk_test (int n)
+{
+  (void)n;
+  mad_desc_newv(6, 1);
+
+  tpsa a(newt());
+  tpsa b(newt());
+
+  mad_tpsa_setvar(&*a, 10, 1, 0);
+  mad_tpsa_setvar(&*b, 20, 1, 0);
+
+//  (ref(a)) = 10;
+
+  tpsa c = (a+1)*(b+2)+2;
+  tpsa d = a+1+a+2+a+3;
+
+//mad_tpsa_print(&*a, "A", 0,0,0);
+//mad_tpsa_print(&*b, "B", 0,0,0);
+//mad_tpsa_print(&*c, "C", 0,0,0);
+//mad_tpsa_print(&*d, "C", 0,0,0);
+
+/*
+> MAD._C.mad_trk_test()
+a
+mad_tpsa.hpp:117:        newt: void
+mad_tpsa.hpp: 61:   tpsa_tmp_: tpsa_t* 0x7fbdb8062608
+
+b
+mad_tpsa.hpp:117:        newt: void
+mad_tpsa.hpp: 61:   tpsa_tmp_: tpsa_t* 0x7fbdb8062e08
+
+a+1
+mad_tpsa.hpp:200:   operator+: tpa,num
+mad_tpsa.hpp: 69:    tpsa_ref: tpsa_t& 0x7fbdb8062e08
+mad_tpsa.hpp:183:   operator+: ref,num
+mad_tpsa.hpp:127:        newt: ref
+mad_tpsa.hpp: 61:   tpsa_tmp_: tpsa_t* 0x7fbdb8063608
+
+b+2
+mad_tpsa.hpp:200:   operator+: tpa,num
+mad_tpsa.hpp: 69:    tpsa_ref: tpsa_t& 0x7fbdb8062608
+mad_tpsa.hpp:183:   operator+: ref,num
+mad_tpsa.hpp:127:        newt: ref
+mad_tpsa.hpp: 61:   tpsa_tmp_: tpsa_t* 0x7fbdb8063e08
+
+(a+1)*(b+2)
+mad_tpsa.hpp:316:   operator*: tpa,tpa                 <---
+mad_tpsa.hpp: 69:    tpsa_ref: tpsa_t& 0x7fbdb8063608
+mad_tpsa.hpp: 69:    tpsa_ref: tpsa_t& 0x7fbdb8063e08
+mad_tpsa.hpp:275:   operator*: ref,ref
+mad_tpsa.hpp:127:        newt: ref
+mad_tpsa.hpp: 61:   tpsa_tmp_: tpsa_t* 0x7fbdb8808208
+
+(a+1)*(b+2) + 2
+mad_tpsa.hpp:200:   operator+: tpa,num
+mad_tpsa.hpp: 69:    tpsa_ref: tpsa_t& 0x7fd8e5064608
+mad_tpsa.hpp:183:   operator+: ref,num
+mad_tpsa.hpp:127:        newt: ref
+mad_tpsa.hpp: 61:   tpsa_tmp_: tpsa_t* 0x7fd8e6008208
+
+dtor
+mad_tpsa.hpp: 52:  operator(): tpsa_t* 0x7fd8e5064608
+mad_tpsa.hpp: 52:  operator(): tpsa_t* 0x7fd8e5063e08
+mad_tpsa.hpp: 52:  operator(): tpsa_t* 0x7fd8e5063608
+
+mad_tpsa.hpp: 52:  operator(): tpsa_t* 0x7fd8e6008208
+mad_tpsa.hpp: 52:  operator(): tpsa_t* 0x7fd8e5062e08
+mad_tpsa.hpp: 52:  operator(): tpsa_t* 0x7fd8e5062608
+
+----
+mad_tpsa.hpp:115:        newt: void
+mad_tpsa.hpp: 59:   tpsa_tmp_: tpsa_t* 0x7fabc8062608
+mad_tpsa.hpp:198:   operator+: tpa,num
+mad_tpsa.hpp: 67:    tpsa_ref: tpsa_t& 0x7fabc8062608
+mad_tpsa.hpp:181:   operator+: ref,num
+mad_tpsa.hpp:125:        newt: ref
+mad_tpsa.hpp: 59:   tpsa_tmp_: tpsa_t* 0x7fabc8062e08
+mad_tpsa.hpp:495:         sqr: tmp
+mad_tpsa.hpp:314:   operator*: tpa,tpa
+mad_tpsa.hpp: 67:    tpsa_ref: tpsa_t& 0x7fabc8062e08
+mad_tpsa.hpp: 67:    tpsa_ref: tpsa_t& 0x7fabc8062e08
+mad_tpsa.hpp:273:   operator*: ref,ref
+mad_tpsa.hpp:125:        newt: ref
+mad_tpsa.hpp: 59:   tpsa_tmp_: tpsa_t* 0x7fabc8063608
+mad_tpsa.hpp: 50:  operator(): tpsa_t* 0x7fabc8062e08
+mad_tpsa.hpp: 50:  operator(): tpsa_t* 0x7fabc8063608
+mad_tpsa.hpp: 50:  operator(): tpsa_t* 0x7fabc8062608
+*/
+
 }
 
 // ----------------------------------------------------------------------------o
