28e965518c6f3f53e0f446728c6f6b060462c209
Laurent Deniau <laurent.deniau@gmail.com>
2024-03-06 18:58:45 +0100

    replace setvar by set0 when possible (not anymore valid)

--- src/mad_tpsa_comp.c
+++ src/mad_tpsa_comp.c
@@ -157,12 +157,12 @@ FUN(eval) (ssz_t sa, const T *ma[sa], ssz_t sb, const NUM tb[sb], NUM tc[sa])
   mad_alloc_tmp(      T*, mc, sa);
   FOR(ib,sb) {
     T *t = FUN(newd)(ma[0]->d, 0);
-    FUN(setvar)(t, tb[ib], 0, 0);
+    FUN(set0)(t, 0, tb[ib]);
     mb[ib] = t;
   }
   FOR(ic,sa) {
     T *t = FUN(newd)(ma[0]->d, 0);
-    FUN(setvar)(t, tc[ic], 0, 0);
+    FUN(set0)(t, 0, tc[ic]);
     mc[ic] = t;
   }
 
