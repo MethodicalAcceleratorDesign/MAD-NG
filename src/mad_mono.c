b93ca0eb4a4baf120e1d6abc21cafda163cd2f27
Laurent Deniau <laurent.deniau@gmail.com>
2024-03-19 15:03:32 +0100

    replace explicit loop by FOR loops

--- src/mad_mono.c
+++ src/mad_mono.c
@@ -30,7 +30,7 @@ ssz_t
 mad_mono_str (ssz_t n, ord_t a[n], str_t s)
 {
   assert(a && s);
-  ssz_t i = 0;
+  idx_t i = 0;
   for (; i < n && s[i]; ++i)
     a[i] = s[i] - (s[i] < 'A' ? '0' : (s[i] < 'a' ? 'A'-10 : 'a'-36));
   return i;
@@ -40,8 +40,7 @@ str_t
 mad_mono_prt (ssz_t n, const ord_t a[n], char s[n+1])
 {
   assert(a && s);
-  for (ssz_t i=0; i < n; ++i)
-    s[i] = a[i] + (a[i] < 10 ? '0' : (a[i] < 36 ? 'A'-10 : 'a'-36));
+  FOR(i,n) s[i] = a[i] + (a[i] < 10 ? '0' : (a[i] < 36 ? 'A'-10 : 'a'-36));
   s[n] = '\0';
   return s;
 }
@@ -50,14 +49,14 @@ void
 mad_mono_fill (ssz_t n, ord_t a[n], ord_t v)
 {
   assert(a);
-  for (idx_t i=0; i < n; ++i) a[i] = v;
+  FOR(i,n) a[i] = v;
 }
 
 void
 mad_mono_copy (ssz_t n, const ord_t a[n], ord_t r[n])
 {
   assert(a && r);
-  for (idx_t i=0; i < n; ++i) r[i] = a[i];
+  FOR(i,n) r[i] = a[i];
 }
 
 ord_t
@@ -65,7 +64,7 @@ mad_mono_min (ssz_t n, const ord_t a[n])
 {
   assert(a);
   ord_t mo = ~0;
-  for (idx_t i=0; i < n; ++i) if (a[i] < mo) mo = a[i];
+  FOR(i,n) if (a[i] < mo) mo = a[i];
   return mo;
 }
 
@@ -74,7 +73,7 @@ mad_mono_max (ssz_t n, const ord_t a[n])
 {
   assert(a);
   ord_t mo = 0;
-  for (idx_t i=0; i < n; ++i) if (a[i] > mo) mo = a[i];
+  FOR(i,n) if (a[i] > mo) mo = a[i];
   return mo;
 }
 
@@ -83,7 +82,7 @@ mad_mono_ord (ssz_t n, const ord_t a[n])
 {
   assert(a);
   int s = 0;
-  for (idx_t i=0; i < n; ++i) s += a[i];
+  FOR(i,n) s += a[i];
   return s;
 }
 
@@ -93,7 +92,7 @@ mad_mono_ordp (ssz_t n, const ord_t a[n], idx_t stp)
   assert(a);
   ensure(stp >= 1, "invalid step %d (>= 1)", stp);
   num_t p = 1;
-  for (idx_t i=0; i < n; i+=stp) p *= a[i];
+  FOR(i,0,n,stp) p *= a[i];
   return p;
 }
 
@@ -103,7 +102,7 @@ mad_mono_ordpf (ssz_t n, const ord_t a[n], idx_t stp)
   assert(a);
   ensure(stp >= 1, "invalid step %d (>= 1)", stp);
   num_t p = 1;
-  for (idx_t i=0; i < n; i+=stp) p *= mad_num_fact(a[i]);
+  FOR(i,0,n,stp) p *= mad_num_fact(a[i]);
   return p;
 }
 
@@ -111,7 +110,7 @@ log_t
 mad_mono_eq (ssz_t n, const ord_t a[n], const ord_t b[n])
 {
   assert(a && b);
-  for (idx_t i=0; i < n; ++i) if (a[i] != b[i]) return FALSE;
+  FOR(i,n) if (a[i] != b[i]) return FALSE;
   return TRUE;
 }
 
@@ -119,7 +118,7 @@ log_t
 mad_mono_lt (ssz_t n, const ord_t a[n], const ord_t b[n])
 {
   assert(a && b);
-  for (idx_t i=0; i < n; ++i) if (a[i] >= b[i]) return FALSE;
+  FOR(i,n) if (a[i] >= b[i]) return FALSE;
   return TRUE;
 }
 
@@ -127,7 +126,7 @@ log_t
 mad_mono_le (ssz_t n, const ord_t a[n], const ord_t b[n])
 {
   assert(a && b);
-  for (idx_t i=0; i < n; ++i) if (a[i] > b[i]) return FALSE;
+  FOR(i,n) if (a[i] > b[i]) return FALSE;
   return TRUE;
 }
 
@@ -135,7 +134,7 @@ int
 mad_mono_cmp (ssz_t n, const ord_t a[n], const ord_t b[n])
 {
   assert(a && b);
-  for (idx_t i=0; i < n; ++i) if (a[i] != b[i]) return (int)a[i] - b[i];
+  FOR(i,n) if (a[i] != b[i]) return (int)a[i] - b[i];
   return 0;
 }
 
@@ -143,7 +142,7 @@ int
 mad_mono_rcmp (ssz_t n, const ord_t a[n], const ord_t b[n])
 {
   assert(a && b);
-  for (idx_t i=n-1; i >= 0; --i) if (a[i] != b[i]) return (int)a[i] - b[i];
+  RFOR(i,n) if (a[i] != b[i]) return (int)a[i] - b[i];
   return 0;
 }
 
@@ -151,14 +150,14 @@ void
 mad_mono_add (ssz_t n, const ord_t a[n], const ord_t b[n], ord_t r[n])
 {
   assert(a && b && r);
-  for (idx_t i=0; i < n; ++i) r[i] = a[i] + b[i];
+  FOR(i,n) r[i] = a[i] + b[i];
 }
 
 void
 mad_mono_sub (ssz_t n, const ord_t a[n], const ord_t b[n], ord_t r[n])
 {
   assert(a && b && r);
-  for (idx_t i=0; i < n; ++i) r[i] = a[i] > b[i] ? a[i] - b[i] : 0;
+  FOR(i,n) r[i] = a[i] > b[i] ? a[i] - b[i] : 0;
 }
 
 void
@@ -174,11 +173,8 @@ mad_mono_rev (ssz_t n, const ord_t a[n], ord_t r[n])
 {
   assert(a && r);
   ord_t t;
-  if (a != r)
-    for (idx_t i=0; i < n; ++i) r[i] = a[n-1-i];
-  else
-    for (idx_t i=0; i < n/2; ++i)
-      t = r[i], r[i] = r[n-1-i], r[n-1-i] = t; // swap
+  if (a != r) FOR(i,n)           r[i] = a[n-1-i];
+  else        FOR(i,n/2) t=r[i], r[i] = r[n-1-i], r[n-1-i]=t; // swap
 }
 
 
@@ -190,9 +186,9 @@ mad_mono_print (ssz_t n, const ord_t a[n], FILE *fp_)
   assert(a);
   if (!fp_) fp_ = stdout;
 
-  fprintf(fp_, "[ ");
-  for (idx_t i=0; i < n; ++i) fprintf(fp_, "%d ", a[i]);
-  fprintf(fp_, "]");
+           fprintf(fp_, "[ ");
+  FOR(i,n) fprintf(fp_, "%d ", a[i]);
+           fprintf(fp_, "]");
 }
 
 // --- end --------------------------------------------------------------------o
