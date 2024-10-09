dc8c7725d5cf53f82eab06deadae2be774e16741
Laurent Deniau <laurent.deniau@gmail.com>
2024-10-09 15:20:00 +0200

    restore constants header, disable DBG_SERIES

--- src/mad_tpsa_fun.c
+++ src/mad_tpsa_fun.c
@@ -22,6 +22,7 @@
 #include <complex.h>
 
 #include "mad_log.h"
+#include "mad_cst.h"
 #include "mad_num.h"
 #include "mad_tpsa_impl.h"
 #include "mad_ctpsa_impl.h"
@@ -29,7 +30,7 @@
 // --- local ------------------------------------------------------------------o
 
 #define OLD_SERIES 0
-#define DBG_SERIES 1
+#define DBG_SERIES 0
 
 enum { MANUAL_EXPANSION_ORD = 6 };
 
